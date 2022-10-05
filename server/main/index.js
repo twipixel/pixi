6e9a3d0ae819c1f6313e61041554b6e2ba8272ad
choi.sungryeol <choi.sungryeol@navercorp.com>
2022-10-05 23:51:39 +0900

    add display object image

--- server/main/index.js
+++ server/main/index.js
@@ -3,6 +3,7 @@ var express = express || require('express'),
   env = process.env.NODE_ENV || 'development';
 
 router.use('/dist', express.static('dist'));
+router.use('/asset', express.static('asset'));
 router.use('/practice', express.static('practice'));
 router.use('/samples', express.static('samples'));
 router.use(express.static('server/public'));
