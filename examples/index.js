f3d567e28ad2f3c66ffd4d32fd469740a961e9b4
choi.sungryeol <choi.sungryeol@navercorp.com>
2022-10-23 21:37:37 +0900

    fix create examples

---
+++ examples/index.js
@@ -0,0 +1,3 @@
+import {files} from './examples.js';
+
+
--- scripts/create-examples.js
+++ scripts/create-examples.js
@@ -1,31 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const getExamples = (dirPath, examples = {}, files = []) => {
+const getExamples = (dirPath, examples = {}, files = [], depth = 0) => {
   const readFiles = fs.readdirSync(dirPath);
 
   readFiles.forEach(file => {
     if (fs.statSync(dirPath + '/' + file).isDirectory()) {
       examples[file] = {};
-      getExamples(dirPath + '/' + file, examples[file], files);
+      getExamples(dirPath + '/' + file, examples[file], files, depth + 1);
     } else {
-      if (path.extname(file).toLowerCase() === '.html') {
+      if (path.extname(file).toLowerCase() === '.html' && !(depth === 0 && file.toLowerCase().indexOf('index') !== -1)) {
         const filePath = path.join(__dirname, dirPath, '/', file).replace(__dirname, '');
         files.push(filePath);
         examples[file] = filePath;
       }
     }
   })
-
   return { examples, files }
 }
 
-const json = getExamples('./examples');
-const jsonString = JSON.stringify(json, null, 2);
-console.log(jsonString);
+const { examples, files } = getExamples('./examples');
 fs.writeFile(
-  './examples/examples.json',
-  jsonString,
+  './examples/examples.js',
+  `export const examples = ${JSON.stringify(examples, null, 2)};
+  export const files = ${JSON.stringify(files, null, 2)};`,
   { flags: 'wx', encoding: 'utf8' },
   (err) => console.log(err ? err.toString() : 'create examples complete')
 );
