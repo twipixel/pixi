3982fa96afb0ceba401cd630045b37e346e06449
choi.sungryeol <choi.sungryeol@navercorp.com>
2022-10-24 11:01:44 +0900

    implementing example page

--- examples/index.js
+++ examples/index.js
@@ -8,31 +8,32 @@ for (let i = 0; i < files.length; i+=1) {
   const paths = url.split('/');
   let [category, example, subExample] = paths.filter(path => path && path !== 'examples');
 
-  const title = (() => {
+  const {title, subTitle} = (() => {
     example = example.toLowerCase();
     example = example.replaceAll('-', ' ');
     const htmlIndex = example.indexOf('.html');
 
     // 카테고리에 바로 예제가 있는 경우
-    if (htmlIndex !== -1) return example.substring(0, htmlIndex);
+    if (htmlIndex !== -1) return {title: example.substring(0, htmlIndex)};
 
     subExample = subExample.toLowerCase();
     // 예제 안에 예제가 없는 경우
-    if (subExample.includes('.html')) return example;
+    if (subExample.includes('.html')) return {title: example};
 
     // 예제 안에 보조 예제가 있는 경우
-    return `${example} ${subExampleSeperator} ${subExample.replaceAll('-', ' ')}`
+    return {title: example, subTitle: subExample.replaceAll('-', ' ')};
   })();
 
   const item = document.createElement('a');
-  item.href = url;
-  item.target = '_blank';
   const titleField = document.createElement('p');
   const categoryField = document.createElement('p');
   item.appendChild(titleField);
   item.appendChild(categoryField);
   container.appendChild(item);
 
+  item.id = 'item';
+  item.href = url;
+  item.target = '_blank';
 
   categoryField.innerText = category;
   titleField.innerText = title;
