be7fe163b4ad611e05e9f60776cab853dac6e5fb
최성렬[Maps Service FE] <choi.sungryeol@navercorp.com>
2023-12-10 23:53:30 +0900

    예제 정리

--- examples/examples.js
+++ examples/examples.js
@@ -1,341 +1,75 @@
 
   export const examples = {
-  "cmiscm": {
-    "colliding-ball": {
-      "index": {
-        "path": "/examples/cmiscm/colliding-ball/index.html",
-        "modifyTime": 1665618266153.8384
-      }
-    },
-    "dot-painting": {
-      "dot-painting-bw": {
-        "index": {
-          "path": "/examples/cmiscm/dot-painting/dot-painting-bw/index.html",
-          "modifyTime": 1665589773218.6997
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/dot-painting/index.html",
-        "modifyTime": 1665589768542.609
-      }
-    },
-    "glow-gradient": {
-      "basic": {
-        "index": {
-          "path": "/examples/cmiscm/glow-gradient/basic/index.html",
-          "modifyTime": 1665586574906.746
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/glow-gradient/index.html",
-        "modifyTime": 1665590066944.0305
-      }
-    },
-    "kinetic-typo-1": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-1/index.html",
-        "modifyTime": 1665549179730.437
-      }
-    },
-    "kinetic-typo-2": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-2/index.html",
-        "modifyTime": 1666397855743.8347
-      }
-    },
-    "kinetic-typo-3": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-3/index.html",
-        "modifyTime": 1666270116295.6633
-      }
-    },
-    "kinetic-typo-4": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-4/index.html",
-        "modifyTime": 1666220942618.758
-      }
-    },
-    "kinetic-typo-5": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-5/index.html",
-        "modifyTime": 1666196114281.8875
-      }
-    },
-    "kinetic-typo-6": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-6/index.html",
-        "modifyTime": 1665647729741.1497
-      }
-    },
-    "kinetic-typo-7": {
-      "index": {
-        "path": "/examples/cmiscm/kinetic-typo-7/index.html",
-        "modifyTime": 1665644366451.1787
-      }
-    },
-    "line-string": {
-      "basic": {
-        "index": {
-          "path": "/examples/cmiscm/line-string/basic/index.html",
-          "modifyTime": 1665418318421.7104
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/line-string/index.html",
-        "modifyTime": 1665418307643.0378
-      }
-    },
-    "rotate": {
-      "circle": {
-        "index": {
-          "path": "/examples/cmiscm/rotate/circle/index.html",
-          "modifyTime": 1665626918836.4138
-        }
-      },
-      "circle-box": {
-        "index": {
-          "path": "/examples/cmiscm/rotate/circle-box/index.html",
-          "modifyTime": 1665628031179.064
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/rotate/index.html",
-        "modifyTime": 1665625034477.2634
-      },
-      "polygon": {
-        "index": {
-          "path": "/examples/cmiscm/rotate/polygon/index.html",
-          "modifyTime": 1665626241596.7332
-        }
-      }
-    },
-    "sheep": {
-      "basic": {
-        "index": {
-          "path": "/examples/cmiscm/sheep/basic/index.html",
-          "modifyTime": 1665384776112.5398
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/sheep/index.html",
-        "modifyTime": 1665191355950.0742
-      }
-    },
-    "swing-box": {
-      "index": {
-        "path": "/examples/cmiscm/swing-box/index.html",
-        "modifyTime": 1665421498784.3352
-      }
-    },
-    "wave": {
-      "basic": {
-        "index": {
-          "path": "/examples/cmiscm/wave/basic/index.html",
-          "modifyTime": 1665587998021.9604
-        }
-      },
-      "index": {
-        "path": "/examples/cmiscm/wave/index.html",
-        "modifyTime": 1665589680738.4167
-      },
-      "single": {
-        "index": {
-          "path": "/examples/cmiscm/wave/single/index.html",
-          "modifyTime": 1665588533247.906
-        }
-      }
-    }
-  },
   "pixi": {
     "graphics-canvas": {
       "path": "/examples/pixi/graphics-canvas.html",
-      "modifyTime": 1665190320911.2444
+      "modifyTime": 1701341835428.234
     }
   },
   "practice": {
     "display-object-basic": {
       "path": "/examples/practice/display-object-basic.html",
-      "modifyTime": 1665019628984.4495
+      "modifyTime": 1701341835428.3616
     },
     "display-object-click": {
       "path": "/examples/practice/display-object-click.html",
-      "modifyTime": 1665019633222.1174
+      "modifyTime": 1701341835428.4653
     },
     "display-object-image": {
       "path": "/examples/practice/display-object-image.html",
-      "modifyTime": 1665019638777.7676
+      "modifyTime": 1701341835428.5806
     },
     "display-object-multi": {
       "path": "/examples/practice/display-object-multi.html",
-      "modifyTime": 1665019642713.5298
+      "modifyTime": 1701341835428.714
     },
     "event": {
       "path": "/examples/practice/event.html",
-      "modifyTime": 1665043503068.3418
+      "modifyTime": 1701341835428.8137
     },
     "kvo": {
       "path": "/examples/practice/kvo.html",
-      "modifyTime": 1665019672729.3523
+      "modifyTime": 1701341835428.903
     }
   }
 };
   export const files = [
   {
-    "path": "/examples/cmiscm/kinetic-typo-2/index.html",
-    "modifyTime": 1666397855743.8347
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-3/index.html",
-    "modifyTime": 1666270116295.6633
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-4/index.html",
-    "modifyTime": 1666220942618.758
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-5/index.html",
-    "modifyTime": 1666196114281.8875
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-6/index.html",
-    "modifyTime": 1665647729741.1497
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-7/index.html",
-    "modifyTime": 1665644366451.1787
-  },
-  {
-    "path": "/examples/cmiscm/rotate/circle-box/index.html",
-    "modifyTime": 1665628031179.064
-  },
-  {
-    "path": "/examples/cmiscm/rotate/circle/index.html",
-    "modifyTime": 1665626918836.4138
-  },
-  {
-    "path": "/examples/cmiscm/rotate/polygon/index.html",
-    "modifyTime": 1665626241596.7332
-  },
-  {
-    "path": "/examples/cmiscm/rotate/index.html",
-    "modifyTime": 1665625034477.2634
-  },
-  {
-    "path": "/examples/cmiscm/colliding-ball/index.html",
-    "modifyTime": 1665618266153.8384
-  },
-  {
-    "path": "/examples/cmiscm/glow-gradient/index.html",
-    "modifyTime": 1665590066944.0305
-  },
-  {
-    "path": "/examples/cmiscm/dot-painting/dot-painting-bw/index.html",
-    "modifyTime": 1665589773218.6997
-  },
-  {
-    "path": "/examples/cmiscm/dot-painting/index.html",
-    "modifyTime": 1665589768542.609
-  },
-  {
-    "path": "/examples/cmiscm/wave/index.html",
-    "modifyTime": 1665589680738.4167
-  },
-  {
-    "path": "/examples/cmiscm/wave/single/index.html",
-    "modifyTime": 1665588533247.906
-  },
-  {
-    "path": "/examples/cmiscm/wave/basic/index.html",
-    "modifyTime": 1665587998021.9604
-  },
-  {
-    "path": "/examples/cmiscm/glow-gradient/basic/index.html",
-    "modifyTime": 1665586574906.746
-  },
-  {
-    "path": "/examples/cmiscm/kinetic-typo-1/index.html",
-    "modifyTime": 1665549179730.437
-  },
-  {
-    "path": "/examples/cmiscm/swing-box/index.html",
-    "modifyTime": 1665421498784.3352
-  },
-  {
-    "path": "/examples/cmiscm/line-string/basic/index.html",
-    "modifyTime": 1665418318421.7104
-  },
-  {
-    "path": "/examples/cmiscm/line-string/index.html",
-    "modifyTime": 1665418307643.0378
-  },
-  {
-    "path": "/examples/cmiscm/sheep/basic/index.html",
-    "modifyTime": 1665384776112.5398
-  },
-  {
-    "path": "/examples/cmiscm/sheep/index.html",
-    "modifyTime": 1665191355950.0742
-  },
-  {
-    "path": "/examples/pixi/graphics-canvas.html",
-    "modifyTime": 1665190320911.2444
+    "path": "/examples/practice/kvo.html",
+    "modifyTime": 1701341835428.903
   },
   {
     "path": "/examples/practice/event.html",
-    "modifyTime": 1665043503068.3418
-  },
-  {
-    "path": "/examples/practice/kvo.html",
-    "modifyTime": 1665019672729.3523
+    "modifyTime": 1701341835428.8137
   },
   {
     "path": "/examples/practice/display-object-multi.html",
-    "modifyTime": 1665019642713.5298
+    "modifyTime": 1701341835428.714
   },
   {
     "path": "/examples/practice/display-object-image.html",
-    "modifyTime": 1665019638777.7676
+    "modifyTime": 1701341835428.5806
   },
   {
     "path": "/examples/practice/display-object-click.html",
-    "modifyTime": 1665019633222.1174
+    "modifyTime": 1701341835428.4653
   },
   {
     "path": "/examples/practice/display-object-basic.html",
-    "modifyTime": 1665019628984.4495
+    "modifyTime": 1701341835428.3616
+  },
+  {
+    "path": "/examples/pixi/graphics-canvas.html",
+    "modifyTime": 1701341835428.234
   }
 ];
   export const paths = [
-  "/examples/cmiscm/kinetic-typo-2/index.html",
-  "/examples/cmiscm/kinetic-typo-3/index.html",
-  "/examples/cmiscm/kinetic-typo-4/index.html",
-  "/examples/cmiscm/kinetic-typo-5/index.html",
-  "/examples/cmiscm/kinetic-typo-6/index.html",
-  "/examples/cmiscm/kinetic-typo-7/index.html",
-  "/examples/cmiscm/rotate/circle-box/index.html",
-  "/examples/cmiscm/rotate/circle/index.html",
-  "/examples/cmiscm/rotate/polygon/index.html",
-  "/examples/cmiscm/rotate/index.html",
-  "/examples/cmiscm/colliding-ball/index.html",
-  "/examples/cmiscm/glow-gradient/index.html",
-  "/examples/cmiscm/dot-painting/dot-painting-bw/index.html",
-  "/examples/cmiscm/dot-painting/index.html",
-  "/examples/cmiscm/wave/index.html",
-  "/examples/cmiscm/wave/single/index.html",
-  "/examples/cmiscm/wave/basic/index.html",
-  "/examples/cmiscm/glow-gradient/basic/index.html",
-  "/examples/cmiscm/kinetic-typo-1/index.html",
-  "/examples/cmiscm/swing-box/index.html",
-  "/examples/cmiscm/line-string/basic/index.html",
-  "/examples/cmiscm/line-string/index.html",
-  "/examples/cmiscm/sheep/basic/index.html",
-  "/examples/cmiscm/sheep/index.html",
-  "/examples/pixi/graphics-canvas.html",
-  "/examples/practice/event.html",
   "/examples/practice/kvo.html",
+  "/examples/practice/event.html",
   "/examples/practice/display-object-multi.html",
   "/examples/practice/display-object-image.html",
   "/examples/practice/display-object-click.html",
-  "/examples/practice/display-object-basic.html"
+  "/examples/practice/display-object-basic.html",
+  "/examples/pixi/graphics-canvas.html"
 ];
   
