f94d0b6f6eb989d5e1510cf66b96d3ea8bab7866
최성렬[Maps Service FE] <choi.sungryeol@navercorp.com>
2023-12-11 20:54:09 +0900

    eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,27 @@
 module.exports = {
-  env: {
+  "env": {
     browser: true,
     es6: true,
     node: true,
   },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
+  "extends": [
+    "airbnb",
+    "prettier",
+    "plugin:node/recommended"
+  ],
+  "settings": {
+    "import/resolver": "node"
+  },
+  "plugins": [
+    "prettier"
+  ],
+  "rules": {
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off"
+  }
 };
